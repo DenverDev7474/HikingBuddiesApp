a773df3d0a21c5facd8efc5d2fef9add921a893c
Hugo Amorim <hugo.amorim.dev@gmail.com>
2023-06-16 15:24:44 -0600

    revised pull

--- frontend/navigator/CalendarNavigator.js
+++ frontend/navigator/CalendarNavigator.js
@@ -1,79 +1,78 @@
-import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import CalendarScreen from '../screens/Calendar/CalendarScreen';
-import CalendarDetailScreen from '../screens/Calendar/CalendarDetailScreen';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import Colors from '../constants/colors'
-
+import CalendarScreen from "../screens/Calendar/CalendarScreen";
+import CalendarDetailScreen from "../screens/Calendar/CalendarDetailScreen";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import Colors from "../constants/colors";
 
 const Stack = createNativeStackNavigator();
 
 export default function CalendarNavigator({ navigation }) {
-    return (
-    <Stack.Navigator 
-        initialRouteName="CalendarScreen"
-    >
-        <Stack.Screen 
-            name="CalendarScreen" 
-            component={CalendarScreen} 
-            options={{
-                header: (props) =>
-                    (
-                      <View style={{ height: 130, backgroundColor: '#CCE1EF' }}>
-                          <Text style={styles.headerTitle}>Your Hikes</Text>
-                      </View>
-                    ),
-              }} 
-        
-        />
-        <Stack.Screen 
-            name="CalendarDetail" 
-            component={CalendarDetailScreen} 
-            options={{
-                header: (props) =>
-                    (
-                      <View style={{ height: 130, backgroundColor: '#CCE1EF', display:'flex'}}>
-                          <View style={styles.buttonContainer}>  
-                            <TouchableOpacity>
-                                <Text 
-                                    style={styles.buttonText} 
-                                    onPress={() => navigation.navigate('CalendarScreen')}
-                                >
-                                Back
-                                </Text>
-                         </TouchableOpacity>
-                        </View>
-                    </View>
-                    ),
-                }} 
-        
-        />
+  return (
+    <Stack.Navigator initialRouteName="CalendarScreen">
+      <Stack.Screen
+        name="CalendarScreen"
+        component={CalendarScreen}
+        options={{
+          header: (props) => (
+            <View style={{ height: 130, backgroundColor: "#CCE1EF" }}>
+              <Text style={styles.headerTitle}>Your Hikes</Text>
+            </View>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="CalendarDetail"
+        component={CalendarDetailScreen}
+        options={{
+          header: (props) => (
+            <View
+              style={{
+                height: 130,
+                backgroundColor: "#CCE1EF",
+                display: "flex",
+              }}
+            >
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity>
+                  <Text
+                    style={styles.buttonText}
+                    onPress={() => navigation.navigate("CalendarScreen")}
+                  >
+                    Back
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          ),
+        }}
+      />
     </Stack.Navigator>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-    headerTitle: {
-        fontSize: 40,
-        marginLeft: 17,
-        fontFamily: 'robotoSlab',
-        bottom: 0,
-        color: Colors.primaryDarkBlue,
-        top: 70
-    },
-    buttonContainer: {
-        padding:10,
-        backgroundColor: Colors.primaryDarkBlue,
-        borderRadius: 5,
-        width: 60,
-        top: 80,
-        position: 'absolute',
-        right: 17,
-        bottom: 10
-    },
-    buttonText: {
-        color: 'white',
-        textAlign: 'center'
-    },
+  headerTitle: {
+    fontSize: 40,
+    marginLeft: 17,
+    fontFamily: "robotoSlab",
+    bottom: 0,
+    color: Colors.primaryDarkBlue,
+    top: 70,
+  },
+  buttonContainer: {
+    padding: 10,
+    backgroundColor: Colors.primaryDarkBlue,
+    borderRadius: 5,
+    width: 60,
+    top: 80,
+    position: "absolute",
+    right: 17,
+    bottom: 10,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
 });
--- frontend/navigator/TabNavigator.js
+++ frontend/navigator/TabNavigator.js
@@ -1,73 +1,77 @@
-import * as React from 'react';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import HikeNavigator from './HikeNavigator';
-import CalendarNavigator from './CalendarNavigator';
-import InfoNavigator from './InfoNavigator';
-import Colors from '../constants/colors';
+import * as React from "react";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import HikeNavigator from "./HikeNavigator";
+import CalendarNavigator from "./CalendarNavigator";
+import InfoNavigator from "./InfoNavigator";
+import Colors from "../constants/colors";
 
-
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faHiking, faCalendar, faMountain } from '@fortawesome/free-solid-svg-icons'
-import { StyleSheet, Text, View } from 'react-native';
-
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faHiking,
+  faCalendar,
+  faMountain,
+} from "@fortawesome/free-solid-svg-icons";
+import { StyleSheet, Text, View } from "react-native";
 
 const Tab = createMaterialBottomTabNavigator();
 
 export default function TabNavigator() {
-
   return (
-        <Tab.Navigator 
-            initialRouteName='HikeTAB'
-            activeColor={Colors.lightBlue}
-            inactiveColor={Colors.secondaryGreen}
-            barStyle={{  
-                backgroundColor: Colors.primaryDarkBlue,
-                height: 93
-            }}
-            >
-            <Tab.Screen 
-                name="HikeTAB" 
-                component={HikeNavigator}  
-                options={{
-                    tabBarLabel: <Text style={styles.barLabel}>HIKE</Text>,
-                    tabBarIcon: ({ color }) => (
-                        <View style={styles.barIcon}>
-                            <FontAwesomeIcon icon={ faHiking } style={styles.icon} size={30} color={color} />
-                        </View>
-                    ),
-                }}
-            />
-            <Tab.Screen 
-                name="CalendarTAB" 
-                component={CalendarNavigator} 
-                options={{
-                    tabBarLabel: <Text style={styles.barLabel}>CALENDAR</Text>,
-                    tabBarIcon: ({color}) => (
-                    
-                        <FontAwesomeIcon icon={ faCalendar } size={30} color={color}/>
-                    ),
-                }}
-                
-            />
-            <Tab.Screen 
-                name="InfoTAB" 
-                component={InfoNavigator} 
-                options={{
-                    tabBarLabel: <Text style={styles.barLabel}>INFO</Text>,
-                    tabBarIcon: ({color}) => (
-                        <FontAwesomeIcon icon={ faMountain } size={30} color={color}/>
-                    )
-                }}
-            />
-        </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="HikeTAB"
+      activeColor={Colors.lightBlue}
+      inactiveColor={Colors.secondaryGreen}
+      barStyle={{
+        backgroundColor: Colors.primaryDarkBlue,
+        height: 93,
+      }}
+    >
+      <Tab.Screen
+        name="HikeTAB"
+        component={HikeNavigator}
+        options={{
+          tabBarLabel: <Text style={styles.barLabel}>HIKE</Text>,
+          tabBarIcon: ({ color }) => (
+            <View style={styles.barIcon}>
+              <FontAwesomeIcon
+                icon={faHiking}
+                style={styles.icon}
+                size={30}
+                color={color}
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="CalendarTAB"
+        component={CalendarNavigator}
+        options={{
+          tabBarLabel: <Text style={styles.barLabel}>CALENDAR</Text>,
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faCalendar} size={30} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="InfoTAB"
+        component={InfoNavigator}
+        options={{
+          tabBarLabel: <Text style={styles.barLabel}>INFO</Text>,
+          tabBarIcon: ({ color }) => (
+            <FontAwesomeIcon icon={faMountain} size={30} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 }
 
 const styles = StyleSheet.create({
-    barLabel: {
-        fontSize: 15,
-        fontFamily: 'robotoSlabLight',
-        fontWeight: '500', 
-        lineHeight: 35
-    }
+  barLabel: {
+    fontSize: 15,
+    fontFamily: "robotoSlabLight",
+    fontWeight: "500",
+    lineHeight: 35,
+  },
 });
