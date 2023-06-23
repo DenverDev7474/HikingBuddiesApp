7a444813df09ed3aab8ad948baa7e595f2e8d1d3
Hugo Amorim <hugo.amorim.dev@gmail.com>
2023-06-23 16:58:26 -0600

    major fix

--- backend/routers/usersRouter.js
+++ backend/routers/usersRouter.js
@@ -7,24 +7,13 @@ const authController = require('../controllers/authController.js');
 router.post('/login', authController.login);
 
 router.get('/user/:id', userController.getUserById);
+
 router.post('/register', 
-    userController.validateRegister, 
+    // userController.validateRegister, 
     userController.register, 
 );  
 
 router.get('/logout', authController.logout);
 
-
-// router.get('/account', authController.isLoggedIn, userController.getAccount);
-
-// router.post('/account', catchErrors(userController.updateAccount));
-
-// router.post('/account/forgot', catchErrors(authController.forgot));
-
-// router.get('/account/reset/:token', catchErrors(authController.reset));
-
-// router.post('/account/reset/:token', authController.confirmedPasswords, 
-//  catchErrors(authController.update));
-
-
 module.exports = router;
+
