8d8e54122cce0eb4157f2938933bf46222fd85a7
Hugo Amorim <hugo.amorim.dev@gmail.com>
2023-06-26 14:07:49 -0600

    fixed git

--- frontend/constants/colors.js
+++
@@ -1,9 +0,0 @@
-export default Color ={
-    primaryDarkBlue: '#072649',
-    secondaryGreen: '#9FB52A', 
-    secondaryBlue: '#0068B0',
-    lightBlue: '#88CDEF',
-    backgroundGreen: '#D8E1A9',
-    backgroundBlue: '#CCE1EF'
-}
-
